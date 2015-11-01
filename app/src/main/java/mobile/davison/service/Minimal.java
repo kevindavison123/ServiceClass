1551a8e3a60a84c23f63e3717f04fbafa63b156f
Kevin <kevindavison123@gmail.com>
2015-11-01 16:50:04 -0500

    adding button functionality

--- app/src/main/java/mobile/davison/service/Minimal.java
+++ app/src/main/java/mobile/davison/service/Minimal.java
@@ -1,20 +1,75 @@
 package mobile.davison.service;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Minimal extends AppCompatActivity {
 
     private String tag = "Login";
+    EditText uname;
+    EditText password;
+    Button signIn;
+    Button register;
+    Button forgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minimal);
-
         App.setContext(this);
+        uname = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        signIn = (Button) findViewById(R.id.login);
+        register = (Button) findViewById(R.id.lRegister);
+        forgotPassword = (Button) findViewById(R.id.forgotPass);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent register = new Intent(v.getContext(),Registration.class);
+                startActivityForResult(register,0);
+                finish();
+            }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent forget = new Intent(v.getContext(),Registration.class);
+            }
+        });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!uname.getText().toString().equals("") && !password.getText().toString().equals(""))
+                {
+                    new LoginGetAsync().execute(uname.getText().toString(),password.getText().toString());
+
+                    new LoginPostAsync().execute(uname.getText().toString(),password.getText().toString());
+
+                }
+                if(uname.getText().toString().equals("") && password.getText().toString().equals(""))
+                {
+                    Toast.makeText(getApplicationContext(), "Username and password fields are empty", Toast.LENGTH_SHORT).show();
+                }
+                if(uname.getText().toString().equals(""))
+                {
+                    Toast.makeText(getApplicationContext(),"Username field is empty", Toast.LENGTH_SHORT).show();
+                }
+                if(password.getText().toString().equals(""))
+                {
+                    Toast.makeText(getApplicationContext(),"Password field is empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
--- app/src/main/java/mobile/davison/service/SubmitTest.java
+++ app/src/main/java/mobile/davison/service/SubmitTest.java
@@ -11,6 +11,7 @@ public class SubmitTest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit_test);
+        App.setContext(this);
     }
 
     @Override
